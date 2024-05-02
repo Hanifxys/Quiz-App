3f9e5f95e115b835aa4af86faadb332784ff164b
Hanifxys <mhmdhanif784@gmail.com>
2024-05-02 10:57:57 +0700

    Add Footer component with elegant white color scheme

---
+++ components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-black py-6 text-white text-center fixed bottom-0 w-full">
+      <div className="container mx-auto">
+        <p className="text-lg font-bold">Created by: Muhamad Hanif</p>
+        <p className="text-sm mt-2">All Rights Reserved &copy; 2024</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
