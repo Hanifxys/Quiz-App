50d8c760a89e69900145ff55603d7650ea49e722
Hanifxys <mhmdhanif784@gmail.com>
2024-05-02 11:03:05 +0700

    add license

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+
 import QuizCard from "./components/QuizCard";
 
 
@@ -6,6 +7,7 @@ function App() {
     <div className="flex flex-col items-center mt-20 gap-6">
       <h1 className="text-3xl font-bold">Trivia Quiz App</h1>
       <QuizCard />
+      
     </div>
   );
 }
